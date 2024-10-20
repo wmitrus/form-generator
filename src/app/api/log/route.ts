3eb13e45ec3d2fcd923e539281f5525d1d71ae43
Wojciech Mitrus <wmitrus@gmail.com>
2024-10-20 23:08:53 +0200

    refactor(feature): refactor /api/log route to proper handle Error exceptions and test them

--- src/app/api/log/route.ts
+++ src/app/api/log/route.ts
@@ -1,20 +1,28 @@
 import { NextResponse } from 'next/server';
 import { NextRequest } from 'next/server';
 
-import { appendFile } from 'fs/promises';
+import { logToFile } from '@/lib/utils';
+import logger from '@/pino';
 
 export async function POST(_req: NextRequest) {
   const json = await _req.json();
-  const body = JSON.stringify(json);
 
   try {
-    const fileName = './logs/client.log';
-    await appendFile(fileName, `\n${body}`, 'utf-8');
+    await logToFile(json);
 
     return NextResponse.json({ data: '', success: true }, { status: 201 });
   } catch (err) {
-    console.log('Error appending data to file', err);
-
-    return NextResponse.json({ err, success: false }, { status: 500 });
+    if (err instanceof Error) {
+      logger.error(err);
+      console.log('Error appending data to file', err);
+      return NextResponse.json({ err: { message: err.message }, success: false }, { status: 500 });
+    } else {
+      logger.error(err);
+      console.log('Unexpected error', err);
+      return NextResponse.json(
+        { err: { message: 'An unexpected error occurred' }, success: false },
+        { status: 500 },
+      );
+    }
   }
 }
